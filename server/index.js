73cbdc68e2f2bdd6a56b76c5a2a0e3e95097dacf
sp9324 <headintheclouds104@gmail.com>
2024-06-01 11:02:31 +0530

    corrected cors

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: '*',
+    origin: 'https://dsa-practice-site-client.vercel.app',
   },
 });
 
