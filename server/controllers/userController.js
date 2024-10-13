ae01bf82da49ebb7d054701a2027ffbc6dd24fc4
yolk23 <sakdalanmiko2@gmail.com>
2024-10-13 22:02:58 +0800

    Added a bit of styling enhancement on Sign Up and Sign In Form

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user.js');
+const { User } = require("../models/user.js");
 
 // exports.createUser = async (req, res) => {
 //   const { name, email, password } = req.body;
@@ -21,11 +21,11 @@ exports.getUserById = async (req, res) => {
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
     res.json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching user' });
+    res.status(500).json({ error: "Error fetching user" });
   }
 };
 
@@ -37,13 +37,13 @@ exports.getUserById = async (req, res) => {
 //       if (!user) {
 //         return res.status(404).json({ error: 'User not found' });
 //       }
-  
+
 //       if (isCorrect) {
 //         // Increment points by 5 if the answer is correct
 //         user.points += 5;
 //         await user.save();
 //       }
-  
+
 //       res.json(user);
 //     } catch (error) {
 //       res.status(500).json({ error: 'Error updating user points' });
